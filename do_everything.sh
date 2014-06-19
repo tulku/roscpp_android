8af4a8b41be5c6eadaa2689ccf29e10498e4c144
Gary Servin <garyservin@gmail.com>
2014-06-19 14:10:22 -0400

    Updated for building the ardrone_autonomy driver

--- do_everything.sh
+++ do_everything.sh
@@ -25,56 +25,64 @@ run_cmd() {
 if [ -z $ANDROID_NDK ] ; then
     die "ANDROID_NDK ENVIRONMENT NOT FOUND!"
 fi
- 
+
+if [ -z $ROS_DISTRO ] ; then
+    die "HOST ROS ENVIRONMENT NOT FOUND! Did you source /opt/ros/indigo/setup.bash"
+fi
+
 [ -d $standalone_toolchain_path ] || run_cmd setup_standalone_toolchain
- 
+
 mkdir -p $prefix/libs
 
 # Start with catkin since we use it to build almost everything else
 [ -d $prefix/target ] || mkdir -p $prefix/target
 export CMAKE_PREFIX_PATH=$prefix/target
- 
+
 [ -e $prefix/android.toolchain.cmake ] || ( cd $prefix && download 'https://raw.github.com/taka-no-me/android-cmake/master/android.toolchain.cmake' && cat $my_loc/files/android.toolchain.cmake.addendum >> $prefix/android.toolchain.cmake)
 export RBA_TOOLCHAIN=$prefix/android.toolchain.cmake
- 
+
 # Now get boost with a specialized build
 [ -d $prefix/libs/boost ] || run_cmd get_boost $prefix/libs
-
 [ -d $prefix/libs/poco-1.4.6p2 ] || run_cmd get_poco $prefix/libs
-[ -d $prefix/libs/bzip2-1.0.6 ] || run_cmd get_bzip2 $prefix/libs
 [ -d $prefix/libs/tinyxml ] || run_cmd get_tinyxml $prefix/libs
-[ -d $prefix/libs/uuid ] || run_cmd get_uuid $prefix/libs
-[ -d $prefix/libs/OpenCV-2.4.6-android-sdk ] || run_cmd get_opencv $prefix/libs
-[ -d $prefix/libs/eigen ] || run_cmd get_eigen $prefix/libs
 [ -d $prefix/libs/catkin ] || run_cmd get_catkin $prefix/libs
 [ -d $prefix/libs/console_bridge ] || run_cmd get_console_bridge $prefix/libs
-[ -d $prefix/libs/eigen ] || run_cmd get_eigen $prefix/libs
-[ -d $prefix/libs/flann ] || run_cmd get_flann $prefix/libs
- 
+[ -d $prefix/libs/yaml-cpp ] || run_cmd get_yaml_cpp $prefix/libs
+
 run_cmd build_catkin $prefix/libs/catkin
 . $prefix/target/setup.bash
-if [ $# == 1 ] || [ $2 != '--skip'] ; then
-	run_cmd get_ros_stuff $prefix/libs
-fi
- 
-run_cmd build_bzip2 $prefix/libs/bzip2
-run_cmd build_tinyxml $prefix/libs/tinyxml
 
+# Remove catkin package information from target since we will be cross-compiling it,
+# otherwise catkin will detect it as duplicate
+rm -rf $prefix/target/share/catkin
+
+if [ $# == 1 ] || [ $2 != '--skip' ] ; then
+    run_cmd get_ros_stuff $prefix/libs
+
+    # Patch image_transport
+    patch -p0 -dN $prefix < patches/image_transport.patch
+    # Patch camera_calibration_parsers
+    patch -p0 -dN $prefix < patches/camera_calibration_parsers.patch
+    # Patch camera_info_manager
+    patch -p0 -dN $prefix < patches/camera_info_manager.patch
+    # Patch ros_comm (Accepted on upstream, need to update rosinstall when new release comes out)
+    patch -p0 -dN $prefix < patches/ros_comm.patch
+    # Patch roslib (Accepted on upstream, need to update rosinstall when new release comes out)
+    patch -p0 -dN $prefix < patches/roslib.patch
+fi
+
+run_cmd build_tinyxml $prefix/libs/tinyxml
 run_cmd copy_boost $prefix/libs/boost
 run_cmd build_poco $prefix/libs/poco-1.4.6p2
-run_cmd build_uuid $prefix/libs/uuid
 run_cmd build_console_bridge $prefix/libs/console_bridge
-run_cmd copy_opencv $prefix/libs/OpenCV-2.4.6-android-sdk
-run_cmd build_eigen $prefix/libs/eigen
-run_cmd build_flann $prefix/libs/flann
-# Get and build pcl in one statement.  Avoids pcl rebuilding for no reason.
-[ -d $prefix/libs/pcl ] || (run_cmd get_pcl $prefix/libs && run_cmd build_pcl $prefix/libs/pcl)
+run_cmd build_yaml_cpp $prefix/libs/yaml-cpp
+
 run_cmd build_cpp
 
 run_cmd setup_ndk_project $prefix/roscpp_android_ndk
 run_cmd create_android_mk $prefix/target/catkin_ws/src $prefix/roscpp_android_ndk
-( cd $prefix && run_cmd sample_app sample_app $prefix/roscpp_android_ndk )
- 
+#( cd $prefix && run_cmd sample_app sample_app $prefix/roscpp_android_ndk )
+
 echo
 echo 'done.'
 echo 'summary of what just happened:'
@@ -82,7 +90,6 @@ echo '  target/      was used to build static libraries for ros software'
 echo '    include/   contains headers'
 echo '    lib/       contains static libraries'
 echo '  roscpp_android_ndk/     is a NDK sub-project that can be imported into an NDK app'
-echo '  sample_app/  is an example of such an app, a native activity that uses tf2'
 echo
 echo 'you might now cd into sample_app/, run "ant debug install", and if an'
 echo 'android emulator is running, the app will be flashed onto it.'
