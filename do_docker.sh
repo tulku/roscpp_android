5e8691aff5f5a5f0cd26481e95a80f952bdab838
Gary Servin <garyservin@gmail.com>
2014-10-31 01:49:51 -0300

    Remove harcoded paths and check if docker is available

--- do_docker.sh
+++ do_docker.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-# TODO: Verify docker is available in this platform
+my_loc="$(cd "$(dirname $0)" && pwd)"
+source $my_loc/utils.sh
+
+if [[ $# -lt 1 ]] ; then
+    output_path=$my_loc"/output"
+else
+    output_path=$1
+fi
+
+# Requires docker 1.3+
+cmd_exists docker || die 'docker was not found'
 
 # Build docker image
 sudo docker build -t rosndk .
 # TODO: Verify successful docker image build
 
-sudo docker run -t -v /home/julian/osrf/roscpp_android:/opt/roscpp_android -v /home/julian/osrf/roscpp_android/output:/opt/roscpp_output -i rosndk /opt/roscpp_android/do_everything.sh /opt/roscpp_output
+echo "Setting output dir to: $2"
+
+sudo docker run -t -v $my_loc:/opt/roscpp_android -v $output_path:/opt/roscpp_output -i rosndk /opt/roscpp_android/do_everything.sh /opt/roscpp_output
