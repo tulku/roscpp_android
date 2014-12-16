6f97b236230adc9d866d3687033ba0ca3098fd12
Gary Servin <garyservin@gmail.com>
2014-12-16 17:56:24 -0300

    Add most of the perception variant packages

--- do_everything.sh
+++ do_everything.sh
@@ -100,6 +100,8 @@ export RBA_TOOLCHAIN=$prefix/android.toolchain.cmake
 [ -d $prefix/libs/assimp-3.1.1 ] || run_cmd get_assimp $prefix/libs
 [ -d $prefix/libs/qhull-2012.1 ] || run_cmd get_qhull $prefix/libs
 [ -d $prefix/libs/octomap-1.6.8 ] || run_cmd get_octomap $prefix/libs
+[ -d $prefix/libs/yaml-cpp ] || run_cmd get_yaml_cpp $prefix/libs
+[ -d $prefix/libs/opencv-2.4.9 ] || run_cmd get_opencv $prefix/libs
 
 [ -f $prefix/target/bin/catkin_make ] || run_cmd build_catkin $prefix/libs/catkin
 . $prefix/target/setup.bash
@@ -135,22 +137,30 @@ if [[ $skip -ne 1 ]] ; then
     # Patch libiconv - Remove 'gets' error
     patch -p0 -N -d $prefix < /opt/roscpp_android/patches/libiconv.patch
 
+    # Patch opencv - Fix installation path
+    patch -p0 -N -d $prefix < /opt/roscpp_android/patches/opencv.patch
+
+
     ## ROS patches
 
     # Patch roscpp - avoid using ifaddrs on Android as it is not natively supported
-    # (TODO: remove once https://github.com/ros/ros_comm/pull/518 is accepted)
+    # TODO: https://github.com/ros/ros_comm/pull/518 merged, need to wait until new version(current 1.11.9-0)
     patch -p0 -N -d $prefix < /opt/roscpp_android/patches/roscpp.patch
 
     # Patch roslz4 - remove python stuff
-    # TODO: remove once https://github.com/ros/ros_comm/pull/521 is accepted
+    # TODO: https://github.com/ros/ros_comm/pull/521 merged, need to wait until new version(current 1.11.9-0)
     patch -p0 -N -d $prefix < /opt/roscpp_android/patches/roslz4.patch
 
+    # Patch xmlrpcpp - Add missing header
+    # TODO: https://github.com/ros/ros_comm/pull/537 merged, need to wati until new version(current 1.11.9-0)
+    patch -p0 -N -d $prefix < /opt/roscpp_android/patches/xmlrpcpp.patch
+
     # Patch dynamic_reconfigure - Create static lib
-    # https://github.com/ros/dynamic_reconfigure/pull/42 merged, need to wait until new version (current 1.5.37)
+    # TODO: https://github.com/ros/dynamic_reconfigure/pull/42 merged, need to wait until new version (current 1.5.37)
     patch -p0 -N -d $prefix < /opt/roscpp_android/patches/dynamic_reconfigure.patch
 
     # Patch class_loader - Create static lib
-    # https://github.com/ros/class_loader/pull/20 merged, need to wait until new version (current 0.3.0)
+    # TODO: https://github.com/ros/class_loader/pull/20 merged, need to wait until new version (current 0.3.0)
     patch -p0 -N -d $prefix < /opt/roscpp_android/patches/class_loader.patch
 
     # Patch roslib - weird issue with rospack.
@@ -166,12 +176,45 @@ if [[ $skip -ne 1 ]] ; then
     patch -p0 -N -d $prefix < /opt/roscpp_android/patches/urdf.patch
 
     # Patch tf - Add ARCHIVE DESTINATION and don't do tests for android and remove Python lib
-    # TODO: remove once https://github.com/ros/geometry/pull/76 is accepted
+    # TODO: https://github.com/ros/geometry/pull/76 merged, need to wait until new version (current 1.11.3)
     patch -p0 -N -d $prefix < /opt/roscpp_android/patches/tf.patch
 
     # Patch tf2_ros - Remove Python dependency
-    # TODO: remove once https://github.com/ros/geometry_experimental/pull/72 is accepted
+    # TODO: https://github.com/ros/geometry_experimental/pull/72 merged, need to wait until new version (current 0.5.6)
     patch -p0 -N -d $prefix < /opt/roscpp_android/patches/tf2_ros.patch
+
+    # Patch laser_assembler - Remove testing for Android
+    # TODO: It seems like there may be a better way to handle the test issues
+    # http://stackoverflow.com/questions/22055741/googletest-for-android-ndk
+    patch -p0 -N -d $prefix < /opt/roscpp_android/patches/laser_assembler.patch
+
+    # Patch laser_filters - Remove testing for Android
+    # TODO: It seems like there may be a better way to handle the test issues
+    # http://stackoverflow.com/questions/22055741/googletest-for-android-ndk
+    # https://source.android.com/reference/com/android/tradefed/testtype/GTest.html
+    patch -p0 -N -d $prefix < /opt/roscpp_android/patches/laser_filters.patch
+
+    # Patch image_transport - Fix tinyxml dependency
+    # TODO: create PR
+    patch -p0 -N -d $prefix < /opt/roscpp_android/patches/image_transport.patch
+
+    # Patch camera_calibration_parsers - Fix yaml-cpp dependency
+    # TODO: create PR
+    patch -p0 -N -d $prefix < /opt/roscpp_android/patches/camera_calibration_parsers.patch
+
+    # Patch camera_info_manager - remove testing for Android
+    # TODO: It seems like there may be a better way to handle the test issues
+    # http://stackoverflow.com/questions/22055741/googletest-for-android-ndk
+    # https://source.android.com/reference/com/android/tradefed/testtype/GTest.html
+    patch -p0 -N -d $prefix < /opt/roscpp_android/patches/camera_info_manager.patch
+
+    # Patch cv_bridge - remove Python dependencies
+    # TODO: https://github.com/ros-perception/vision_opencv/pull/55 merged, need to wait until new version (current 1.11.7)
+    patch -p0 -N -d $prefix < /opt/roscpp_android/patches/cv_bridge.patch
+
+    # Patch image_view - Fix duplicated symbol
+    # TODO: https://github.com/ros-perception/image_pipeline/pull/113 merged, need to wait until new version (current 1.12.11)
+    patch -p0 -N -d $prefix < /opt/roscpp_android/patches/image_view.patch
 
 fi
 
@@ -183,7 +226,7 @@ echo
 [ -f $prefix/target/lib/libuuid.a ] || run_cmd build_uuid $prefix/libs/uuid
 [ -f $prefix/target/lib/libboost_system.a ] || run_cmd copy_boost $prefix/libs/boost
 [ -f $prefix/target/lib/libPocoFoundation.a ] || run_cmd build_poco $prefix/libs/poco-1.4.6p2
-[ -f $prefix/target/lib/libtinyxml.a ] || run_cmd build_tinyxml $prefix/libs/tinyxml
+[ -f $prefix/target/share/tinyxml/cmake/tinyxml-config.cmake ] || run_cmd build_tinyxml $prefix/libs/tinyxml
 [ -f $prefix/target/lib/libconsole_bridge.a ] || run_cmd build_console_bridge $prefix/libs/console_bridge
 [ -f $prefix/target/lib/liblz4.a ] || run_cmd build_lz4 $prefix/libs/lz4-r124/cmake_unofficial
 [ -f $prefix/target/lib/libcurl.a ] || run_cmd build_curl $prefix/libs/curl-7.39.0
@@ -196,6 +239,8 @@ echo
 [ -f $prefix/target/lib/libeigen.a ] || run_cmd build_eigen $prefix/libs/eigen
 [ -f $prefix/target/lib/libqhullstatic.a ] || run_cmd build_qhull $prefix/libs/qhull-2012.1
 [ -f $prefix/target/share/octomap/octomap-config.cmake ] || run_cmd build_octomap $prefix/libs/octomap-1.6.8
+[ -f $prefix/target/lib/libyamlcpp.a ] || run_cmd build_yaml_cpp $prefix/libs/yaml-cpp
+[ -f $prefix/target/lib/libopencv_core.a ] || run_cmd build_opencv $prefix/libs/opencv-2.4.9
 
 echo
 echo -e '\e[34mCross-compiling ROS.\e[39m'
@@ -222,10 +267,6 @@ echo
 sed -i 's/set(libraries "urdf;/set(libraries "/g' $CMAKE_PREFIX_PATH/share/urdf/cmake/urdfConfig.cmake
 
 run_cmd create_android_mk $prefix/target/catkin_ws/src $prefix/roscpp_android_ndk
-
-# JAC: Disabled temporarily and replaced by application-specific Android.mk since
-# the library order resulting from create_android_mk doesn't work
-#cp $my_loc/files/Android.mk.algron $prefix/roscpp_android_ndk/Android.mk
 
 if [[ $debugging -eq 1 ]];then
     sed -i "s/#LOCAL_EXPORT_CFLAGS/LOCAL_EXPORT_CFLAGS/g" $prefix/roscpp_android_ndk/Android.mk
